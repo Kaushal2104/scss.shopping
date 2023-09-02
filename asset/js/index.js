9a088f9906bc1f7050e28655ab77a8d36636ce27
Kaushal2104 <96615251+Kaushal2104@users.noreply.github.com>
2023-09-02 15:10:24 +0530

    shopping site

---
+++ asset/js/index.js
@@ -0,0 +1,9 @@
+function menuOpen() {
+  let NavBar = document.getElementById("Navigation");
+  NavBar.classList.add("active");
+}
+function closeMenu() {
+  let nav = document.getElementById("Navigation");
+  nav.classList.remove("active");
+}
+
